5d06f432b8bd3b7e39aedf08eb5716eb23cd089c
Lucas PC <lucasjosue2004@gmail.com>
2025-11-28 20:21:32 -0300

    Añadiendo los models y los serializer

--- Proyecto/api/models.py
+++ Proyecto/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    Titulo = models.CharField(max_length=200)
+    Descripcion = models.TextField()
+    Fecha = models.DateField
+    Portada = models.URLField(max_length=500)
+    Autor = models.CharField(max_length=200)
+    Paginas = models.PositiveIntegerField()
+#todo lo que tiene los libros

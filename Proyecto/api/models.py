8cc6cb6efe5ff6964a8eb54f86d58171e2efd886
Lucas PC <lucasjosue2004@gmail.com>
2025-11-28 21:44:11 -0300

    Correcion a models y a la base de datos

--- Proyecto/api/models.py
+++ Proyecto/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Libro(models.Model):
     Titulo = models.CharField(max_length=200)
     Descripcion = models.TextField()
-    Fecha = models.DateField
+    Fecha = models.DateField()
     Portada = models.URLField(max_length=500)
     Autor = models.CharField(max_length=200)
     Paginas = models.PositiveIntegerField()

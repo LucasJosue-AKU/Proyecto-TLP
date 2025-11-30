b8b77a93e9ba0db39515ebdfc046a68e7411a44c
Felipe Molina <fmolinav@usm.cl>
2025-11-30 02:51:30 -0300

    Recuperar permiso.py de permisos

--- Proyecto/api/permiso.py
+++ Proyecto/api/permiso.py
@@ -2,7 +2,9 @@ from rest_framework import permissions
 
 class Admin_o_Usuario(permissions.BasePermission):
     def has_permission(self, request, view):
+
         if request.method in permissions.SAFE_METHODS:
-            return request.user and request.user.is_authenticated
+            return True
         
-        return request.user and request.user.is_staff
+        return bool(request.user and request.user.is_staff)
+    

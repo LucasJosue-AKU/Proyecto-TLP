c7cc96d743deb5f5ee6f696480a66ec78ec84081
Felipe Molina <fmolinav@usm.cl>
2025-11-30 01:02:12 -0300

    actualizacion de archivos por problemas de pull

--- Proyecto/core/views.py
+++ Proyecto/core/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import requests
 
 def home(request):
-    return render(request, 'core/home.html')
+
+    url_api = "http://127.0.0.1:8000/api/api/projects/"
+    conexion_api = requests.get(url_api)
+    datos_api = conexion_api.json()
+
+    return render(request, 'core/home.html', {})

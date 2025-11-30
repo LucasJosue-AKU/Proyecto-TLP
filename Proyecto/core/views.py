994d90f1eb7fba30c03e0d3ef8a42068d19ff252
Felipe Molina <fmolinav@usm.cl>
2025-11-30 06:31:34 -0300

    Se crea vista y template informacion que contiene la informacion detallada de un libro

--- Proyecto/core/views.py
+++ Proyecto/core/views.py
@@ -11,10 +11,12 @@ def catalogo(request):
     conexion_api = requests.get(url_api)
     datos_api = conexion_api.json()
     contexto_libros = []
-
+    
+    # Se obtienen las portadas de OpenLibrary
     for libro in datos_api:
         titulo = libro["Titulo"]
         id = libro["id"]
+        print(id)
         url_openLibrary = "https://openlibrary.org/search.json?title={}".format(titulo)
         conexion_openLibrary = requests.get(url_openLibrary)
         datos_openLibrary = conexion_openLibrary.json()
@@ -34,4 +36,30 @@ def catalogo(request):
         
     return render(request, 'core/catalogo.html', {
         'libros' : contexto_libros
+    })
+
+def informacion(request, id):
+
+    url_api = "http://127.0.0.1:8000/api/api/projects/{}".format(id)
+    conexion_api = requests.get(url_api)
+    datos_api = conexion_api.json()
+    
+    titulo = datos_api["Titulo"]
+    url_google_api = "https://www.googleapis.com/books/v1/volumes?q=intitle:{}".format(titulo)
+    conexion_google_api = requests.get(url_google_api)
+    datos_libro = conexion_google_api.json()
+    descripcion = ""
+
+    for item in datos_libro["items"][:10]:
+        if "volumeInfo" in item and "description" in item["volumeInfo"]:
+            descripcion = item["volumeInfo"]["description"]
+            break
+
+    contexto_libro = {
+        "titulo" : titulo,
+        "descripcion" : descripcion,
+    }
+
+    return render(request, 'core/informacion.html', {
+        'libro' : contexto_libro
     })

a7516ad091e2a27d7de0b6257f86d0180d5e992b
Felipe Molina <fmolinav@usm.cl>
2025-11-30 05:52:04 -0300

    Se crea vista home que llama a la api propia y la api externa. Se crean las tarjetas automaticamente y se carga con la portada

--- Proyecto/core/views.py
+++ Proyecto/core/views.py
@@ -6,5 +6,28 @@ def home(request):
     url_api = "http://127.0.0.1:8000/api/api/projects/"
     conexion_api = requests.get(url_api)
     datos_api = conexion_api.json()
+    contexto_libros = []
 
-    return render(request, 'core/home.html', {})
+    for libro in datos_api:
+        titulo = libro["Titulo"]
+        id = libro["id"]
+        url_openLibrary = "https://openlibrary.org/search.json?title={}".format(titulo)
+        conexion_openLibrary = requests.get(url_openLibrary)
+        datos_openLibrary = conexion_openLibrary.json()
+        portada = ""
+
+        for doc in datos_openLibrary["docs"]:
+            if "cover_edition_key" in doc:
+                portada = "https://covers.openlibrary.org/b/olid/{}-L.jpg".format(
+                    doc["cover_edition_key"]
+                )
+                break
+
+        contexto_libros.append({
+            "id" : id,
+            "portada" : portada
+        })
+        
+    return render(request, 'core/home.html', {
+        'libros' : contexto_libros
+    })
